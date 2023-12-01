f9c26fbeb817ed3a7778de448e4ace8a7e5d92e1
alumno <alumno@Instituto.local>
2023-12-01 09:13:52 -0300

    sigue habiendo erroress en el buton
    :/

--- carpet/java.js
+++ carpet/java.js
@@ -1,5 +1,27 @@
-const swith = document.querySelector('.switch');
-swith.addEventListener("click", e => {
+/*
+button.onclick = function(){
+    let swith = document.querySelector('.switch');
+    
+
+}
+   swith.addEventListener("click", e => {
     swith.classList.toggle("active");
     document.body.classList.toggle("active");
 })
+}
+document.body.classList.toggle = 'bg-light'
+*/
+let texto = document.querySelector('#p1')
+let button = document.querySelector('button')
+console.log(texto)
+let textoCambiado = false
+texto.textContent = 'prueba'
+button.onclick = function () {
+    if (textoCambiado == false) {
+        texto.textContent = 'skñdkfjldjfdsljlf'
+        textoCambiado = true
+    } else {
+        texto.innertext = 'ste mundial se fue coronada la seleccion de sudafrica superando a Inglaterra por '
+        textoCambiado = false
+    }
+} 
